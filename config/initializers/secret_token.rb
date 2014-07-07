5b2e0c7c7b435b4439a52b78bb2ed381b389b996
Adrianne M <affirmwealth@gmail.com>
2014-07-07 14:25:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '78e88221e11e0c7e7cf7566ea72b90858e6dee35207cb0a1a3ee00884609660d7a67caf35411369ea583e228c6237cb45cd5ba2ff126f0577831dbe5a06fff0b'
